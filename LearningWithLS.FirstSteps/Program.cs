e0a21da2de04c11e33cd65edf65aa5da26e0522d
Lukáš Salak <lukas.salak1@gmail.com>
2023-04-10 01:56:17 +0200

    Add project files.

---
+++ LearningWithLS.FirstSteps/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace LearningWithLS.FirstSteps
+{
+    class Program
+    { 
+        
+        public static void Main(string[] args)
+        {﻿
+
+            // Implement these :)
+
+            // 0 parsing input to int, basic (integer) math
+            // _0_1
+            // Read values of 2 integer numbers and save them as variables a1, a2. Calculate and display their sum.
+            //
+            // _0_2
+            // Read values of 3 integer numbers and save them as variables a1, a2, a3. Calculate and display their product.
+            //
+            // 1 If
+            // _1_1
+            // Read values of 3 integer numbers, save values as variables a1, a2, a3. Calculate and display the number of
+            // values that are equal to 0.
+            //
+            // _1_2. 
+            // Read values of 4 integer values and save them as variables a1, a2, a3, a4. Using MODULO operation, find the
+            // even ones and calculate and display their sum.
+            //
+            // _1_3. 
+            // Read values of 2 integer numbers. Find and display the one which is lower.
+            //
+            // _1_4. 
+            // Read values of 4 integer numbers. Find and display the one with the highest value.
+            //
+            // _1_5. 
+            // Read value of an integer number X. If X is odd number, then read values of another 5 integer numbers. If X
+            // is the middle one, then calculate and display the sum and the product of the sequence (each on its own line).
+            //
+            // 2 For
+            // _2_1. 
+            // Display all odd numbers between 0 and 100.
+            //
+            // _2_2. 
+            // Display the sum of all even numbers between 0 and 100.
+            //
+            // _2_3. 
+            // Read value of an integer number N. Calculate and display the sum of odd numbers from interval (1;N>.
+            //
+            // _2_4. 
+            // Read values of 100 integer numbers. Calculate and display their sum.
+            //
+            // _2_5. 
+            // Read values of 100 integer numbers. Find and display the one with the highest value.
+            //
+            // _2_6. 
+            // Read values of a real number X and an integer number N. Calculate and display the N-th power of X.
+            //
+            // _2_7. 
+            // Read value of integer number N. Then read values of N integer numbers. Calculate and display how many
+            // numbers greater than 50 you have read.
+            //
+            // _2_8. 
+            // Read values of integer numbers A and B. Calculate and display the sum and the products of integers laying
+            // between the given interval (A; B> (including interval boundaries).
+            //
+            // _2_9. 
+            // Read integer number N. Calculate and display the product of the first N natural numbers that can be divided by 3.
+            //
+            // _2_10. 
+            // Read integer number N. Then read a sequence of N integer numbers. Find the maximal and minimal value
+            // from that sequence and display them. Real numbers, Advanced math
+            //
+            // _2_11. 
+            // Read value of a real number A. Calculate and display the value of an edge, volume and surface of a
+            // cube that has a length of its body diagonal A.
+            //
+            // _2_12. 
+            // Read value of an integer number N. Then read values of N real numbers. Calculate and display the
+            // average of real numbers.
+            //
+            // 3 Nested for loops
+            // _3_1
+            // Read value of an integer number A. Find out and display whether number A is a prime.
+            //
+            // _3_2
+            // Read value of an integer number N. Find and display all prime numbers from interval (1; N>.
+            //
+            // 4 While
+            // _4_1
+            // Read integer values until you encounter number 42. Calculate and display the sum of all numbers you
+            // have read.
+
+        }
+    }
+}
